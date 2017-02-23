c893512ef5ec68a39135707f4f5c3e3db08f0399
MonsieurNiark <robyn.danglos@gmail.com>
2017-02-23 03:18:55 +0100

    Création du dossier

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Bataille Navale</title>
+</head>
+
+<body style="text-align: center">
+<h1>Conçu par Robyn DANGLOS</h1>
+<h2>A partir d'un programme en C</h2>
+</body>
+</html>
